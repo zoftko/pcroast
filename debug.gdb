17b983e6b865ac405cefe234817abf0d2de4392c
CrazyBolillo <antonio@zoftko.com>
2023-07-16 23:52:02 -0600

    Implement FreeRTOS and CYW43
    Support for FreeRTOS and the CYW43 driver have been added. With
    this new setup the micro is able to:
    
    * Blink an LED as a low priority task to signal everything is OK
    * Connect to a WIFI Network
    * Log information through UART

---
+++ debug.gdb
@@ -0,0 +1,8 @@
+# helper set of commands to quickly get into a debugging session, use with --command
+
+set width 0
+set height 0
+set verbose off
+
+target extended-remote localhost:3333
+run
--- pico/pcroast.c
+++ pico/pcroast.c
@@ -1,18 +1,80 @@
+#include <FreeRTOS.h>
+#include <hardware/watchdog.h>
+#include <pico/cyw43_arch.h>
 #include <pico/stdlib.h>
+#include <task.h>
 
-void prvSetupHardware() {
-    stdio_init_all();
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+#include "logging.h"
+#include "wifi.h"
+
+TaskHandle_t wifiTaskHandle;
+TaskHandle_t startupTaskHandle;
+TaskHandle_t blinkLedTaskHandle;
+
+void vApplicationMallocFailedHook(void) { LOG_ERROR("malloc has failed"); }
+
+void vApplicationStackOverflowHook(__unused TaskHandle_t xTask, char *pcTaskName) {
+    LOG_ERROR("stack overflow for task %s", pcTaskName);
+    LOG_ERROR("restarting system");
+    watchdog_enable(1, 1);
+    while (1) continue;
+}
+
+void vBlinkLedTask(__unused void *pvParameters) {
+    while (1) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(20));
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        vTaskDelay(pdMS_TO_TICKS(1980));
+    }
+}
+
+void vStartupTask(__unused void *pvParameters) {
+    LOG_INFO("initializing cyw43 module");
+
+    int res = cyw43_arch_init();
+    LOG_DEBUG("cyw43_arch_init returned %d", res);
+    if (res) {
+        LOG_ERROR("failed to initialize cyw43 module");
+        while (1) continue;
+    }
+    cyw43_arch_enable_sta_mode();
+
+    xTaskCreate(
+        vBlinkLedTask, "BlinkLED", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1,
+        &blinkLedTaskHandle
+    );
+    configASSERT(&blinkLedTaskHandle);
+    LOG_DEBUG("blink task created");
+
+    netif_set_status_callback(netif_default, vNetifStatusCallback);
+    xTaskCreate(
+        vWifiTask, "Wifi", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, &wifiTaskHandle
+    );
+    configASSERT(&wifiTaskHandle);
+    LOG_DEBUG("wifi task created");
+    xTaskNotify(wifiTaskHandle, 0, eNoAction);
+
+    LOG_DEBUG("deleting startup task");
+    vTaskDelete(NULL);
+}
+
+void vLaunch() {
+    xTaskCreate(
+        vStartupTask, "Startup", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2,
+        &startupTaskHandle
+    );
+    configASSERT(&startupTaskHandle);
+
+    LOG_DEBUG("startup task created");
+    LOG_DEBUG("starting scheduler");
+    vTaskStartScheduler();
 }
 
 int main(void) {
-    prvSetupHardware();
+    stdio_init_all();
+    LOG_INFO("starting %s version %s", PROJECT_NAME, PROJECT_VERSION);
 
-    while (1) {
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        sleep_ms(200);
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        sleep_ms(1300);
-    }
+    vLaunch();
+    while (1) continue;
 }
