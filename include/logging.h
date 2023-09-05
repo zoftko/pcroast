7314c08a022072530f02b8f6c6a0fcb2ebe16e05
CrazyBolillo <antonio@zoftko.com>
2023-09-05 15:37:37 -0600

    Remove file, function and code line  from logs
    In such a small program it is kind of obvious where messages are coming
    from, and any context coming from knowing the originating file and
    function does not give that much benefit, therefore the extra info has
    been removed.
    
    On the other hand this removal makes the logs that much cleaner and
    easier to read.

--- include/logging.h
+++ include/logging.h
@@ -6,18 +6,17 @@
 #endif
 
 #define NEWLINE "\n"
-#define LOG_FORMAT "%-7s %s:%s:%d: "
-#define LOG_ARGS(TAG) TAG, __FILE_NAME__, __FUNCTION__, __LINE__
+#define LOG_FORMAT "%-7s: "
 #define LOG_PRINT(message, ...) LOG_PRINT_FUNC(LOG_FORMAT message NEWLINE, __VA_ARGS__)
 
 #if DEBUG_BUILD
-#define LOG_DEBUG(message, args...) LOG_PRINT(message, LOG_ARGS("DEBUG"), ##args)
+#define LOG_DEBUG(message, args...) LOG_PRINT(message, "DEBUG", ##args)
 #else
 #define LOG_DEBUG(message, args...)
 #endif
 
-#define LOG_ERROR(message, args...) LOG_PRINT(message, LOG_ARGS("ERROR"), ##args)
-#define LOG_WARNING(message, args...) LOG_PRINT(message, LOG_ARGS("WARNING"), ##args)
-#define LOG_INFO(message, args...) LOG_PRINT(message, LOG_ARGS("INFO"), ##args)
+#define LOG_ERROR(message, args...) LOG_PRINT(message, "ERROR", ##args)
+#define LOG_WARNING(message, args...) LOG_PRINT(message, "WARNING", ##args)
+#define LOG_INFO(message, args...) LOG_PRINT(message, "INFO", ##args)
 
 #endif  // PCROAST_LOGGING_H
