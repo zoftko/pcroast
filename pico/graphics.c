da04147af02491a9a3e13a873e8d583b778fe04a
CrazyBolillo <antonio@zoftko.com>
2023-09-05 21:34:03 -0600

    Add temperature graph on display
    The graph takes up most of the screen and shows a history of the
    temperature changes throughout time. It is automatically updated
    whenever `graphicsSetTemperature` is called.
    
    The temperature label is still shown on screen but it has been moved, it
    is now shown on the bottom left corner of the screen, its size has also
    been diminished.

--- pico/graphics.c
+++ pico/graphics.c
@@ -1,31 +1,74 @@
 #include "graphics.h"
 
-static GHandle temperatureLabel;
-static char temperatureString[7] = "000.00";
+static GHandle temperatureGraph;
+static GGraphObject graphObj;
 
-static gFont dejaVu20Font;
+static GHandle temperatureLabel;
+static char temperatureString[7] = "      ";
+
+static gFont dejaVu16Font;
+
+static uint8_t xPoint;
+static uint8_t yPoint;
+
+/* clang-format off */
+static GGraphStyle graphStyle = {
+    {GGRAPH_POINT_DOT, 0, Yellow},
+    {GGRAPH_LINE_NONE, 0, Gray},
+    {GGRAPH_LINE_SOLID, 0, White},
+    {GGRAPH_LINE_NONE, 0, White},
+    {GGRAPH_LINE_SOLID, 2, Gray, 32},
+    {GGRAPH_LINE_SOLID, 2, Gray, 25},
+    0  // Flags
+};
+/* clang-format on */
 
 void graphicsInit() {
     gfxInit();
-
-    dejaVu20Font = gdispOpenFont("DejaVuSans20");
-
-    gwinSetDefaultFont(dejaVu20Font);
+    dejaVu16Font = gdispOpenFont("DejaVuSans16");
+    gwinSetDefaultFont(dejaVu16Font);
     gwinSetDefaultStyle(&BlackWidgetStyle, FALSE);
 
     GWidgetInit wi;
     gwinWidgetClearInit(&wi);
-
     wi.g.show = gTrue;
-    wi.g.x = 24;
-    wi.g.y = 62;
+    wi.g.x = 4;
+    wi.g.y = 144;
+    wi.g.height = 16;
+    wi.g.width = 56;
     wi.text = temperatureString;
-
     temperatureLabel = gwinLabelCreate(NULL, &wi);
-    graphicsSetTemperature(0);
+
+    GWindowInit winInit;
+    gwinClearInit(&winInit);
+    winInit.show = gTrue;
+    winInit.x = winInit.y = 0;
+    winInit.width = gdispGetWidth();
+    winInit.height = 140;
+
+    /**
+     * For unknown reasons if the graph's axis are written on this routine, they are cleared.
+     * Therefore xPoint is set to 129, this forces a redraw of the graph when the first temperature
+     * reading is taken and displayed on screen, overcoming this. It seems that axis written
+     * outside of this routine don't disappear.
+     */
+    xPoint = 129;
+    temperatureGraph = gwinGraphCreate(&graphObj, &winInit);
+    gwinGraphSetOrigin(temperatureGraph, 0, 0);
+    gwinGraphSetStyle(temperatureGraph, &graphStyle);
 }
 
 void graphicsSetTemperature(float temperature) {
     sprintf(temperatureString, "%06.2f", temperature);
-    gwinSetText(temperatureLabel, temperatureString, gTrue);
+    gwinSetText(temperatureLabel, temperatureString, gFalse);
+
+    yPoint = (uint8_t)temperature - 25;
+    if (xPoint > 128) {
+        gwinClear(temperatureGraph);
+        gwinGraphDrawAxis(temperatureGraph);
+        xPoint = 0;
+    }
+
+    gwinGraphDrawPoint(temperatureGraph, xPoint, yPoint);
+    xPoint++;
 }
